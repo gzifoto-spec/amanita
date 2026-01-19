352058d9ccad2e7e5b5b45ac7e47e6499e6babb2
gzifoto-spec <gzifoto@gmail.com>
2026-01-19 13:50:50 +0100

    feature: basic exercise architecture, adding App.jsx, Description.jsx and Description.css files ready to work

---
+++ amanita/src/components/Description.jsx
@@ -0,0 +1,35 @@
+import '../styles/Description.css';
+
+const images = import.meta.glob('../assets/img/*.png', {
+    eager: true,
+    import: 'default'
+});
+
+function Description(props) {
+    return (
+        <div className='description-container'>
+            <img
+                className='description-image'
+                src={images[`../assets/img/${props.image}.png`]}
+                alt={`Foto de ${props.name}`}
+            />
+
+            <div className='description-text-contain'>
+                <p className='description-name'>
+                    <strong>{props.name}</strong>
+                </p>
+                <p className='description-use'>
+                    {props.use} 
+                </p>
+                <p className='description-toxic'>
+                    "{props.toxic}"
+                <p className='description-text'>
+                    "{props.text}"
+                </p>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Description;
